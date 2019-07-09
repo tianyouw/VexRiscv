5aa1b247f63f1c1d658c804e79da95beccffaa44
tianyouw <tian.you.w@outlook.com>
2019-07-09 16:32:20 -0400

    Disable cache

--- src/main/scala/vexriscv/demo/Briey.scala
+++ src/main/scala/vexriscv/demo/Briey.scala
@@ -49,52 +49,65 @@ object BrieyConfig{
       ),
       cpuPlugins = ArrayBuffer(
         new PcManagerSimplePlugin(0x80000000l, false),
+        new IBusSimplePlugin(
+          resetVector = 0x00000000l,
+          cmdForkOnSecondStage = false,
+          cmdForkPersistence = false,
+          prediction = DYNAMIC_TARGET,
+          catchAccessFault = false,
+          interfaceKeepData = false,
+          compressedGen = true
+        ),
+        new DBusSimplePlugin(
+          catchAddressMisaligned = true,
+          catchAccessFault = true
+        ),
         //          new IBusSimplePlugin(
         //            interfaceKeepData = false,
         //            catchAccessFault = true
         //          ),
-        new IBusCachedPlugin(
-          resetVector = 0x80000000l,
-          prediction = STATIC,
-          config = InstructionCacheConfig(
-            cacheSize = 4096,
-            bytePerLine =32,
-            wayCount = 1,
-            addressWidth = 32,
-            cpuDataWidth = 32,
-            memDataWidth = 32,
-            catchIllegalAccess = true,
-            catchAccessFault = true,
-            asyncTagMemory = false,
-            twoCycleRam = true,
-            twoCycleCache = true
-          )
-          //            askMemoryTranslation = true,
-          //            memoryTranslatorPortConfig = MemoryTranslatorPortConfig(
-          //              portTlbSize = 4
-          //            )
-        ),
+        // new IBusCachedPlugin(
+        //   resetVector = 0x80000000l,
+        //   prediction = STATIC,
+        //   config = InstructionCacheConfig(
+        //     cacheSize = 4096,
+        //     bytePerLine =32,
+        //     wayCount = 1,
+        //     addressWidth = 32,
+        //     cpuDataWidth = 32,
+        //     memDataWidth = 32,
+        //     catchIllegalAccess = true,
+        //     catchAccessFault = true,
+        //     asyncTagMemory = false,
+        //     twoCycleRam = true,
+        //     twoCycleCache = true
+        //   )
+        //   //            askMemoryTranslation = true,
+        //   //            memoryTranslatorPortConfig = MemoryTranslatorPortConfig(
+        //   //              portTlbSize = 4
+        //   //            )
+        // ),
         //                    new DBusSimplePlugin(
         //                      catchAddressMisaligned = true,
         //                      catchAccessFault = true
         //                    ),
-        new DBusCachedPlugin(
-          config = new DataCacheConfig(
-            cacheSize         = 4096,
-            bytePerLine       = 32,
-            wayCount          = 1,
-            addressWidth      = 32,
-            cpuDataWidth      = 32,
-            memDataWidth      = 32,
-            catchAccessError  = true,
-            catchIllegal      = true,
-            catchUnaligned    = true
-          ),
-          memoryTranslatorPortConfig = null
-          //            memoryTranslatorPortConfig = MemoryTranslatorPortConfig(
-          //              portTlbSize = 6
-          //            )
-        ),
+        // new DBusCachedPlugin(
+        //   config = new DataCacheConfig(
+        //     cacheSize         = 4096,
+        //     bytePerLine       = 32,
+        //     wayCount          = 1,
+        //     addressWidth      = 32,
+        //     cpuDataWidth      = 32,
+        //     memDataWidth      = 32,
+        //     catchAccessError  = true,
+        //     catchIllegal      = true,
+        //     catchUnaligned    = true
+        //   ),
+        //   memoryTranslatorPortConfig = null
+        //   //            memoryTranslatorPortConfig = MemoryTranslatorPortConfig(
+        //   //              portTlbSize = 6
+        //   //            )
+        // ),
         new StaticMemoryTranslatorPlugin(
           ioRange      = _(31 downto 28) === 0xF
         ),
