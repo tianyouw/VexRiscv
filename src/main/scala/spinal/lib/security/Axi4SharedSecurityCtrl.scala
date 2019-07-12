1194e4d54f93c3e6296381e16a426e9960640719
Andrew Jiang <andrew.jiang@edu.uwaterloo.ca>
2019-07-12 00:37:25 -0400

    Initial security control block

---
+++ src/main/scala/spinal/lib/security/Axi4SharedSecurityCtrl.scala
@@ -0,0 +1,47 @@
+package spinal.lib.security
+
+import spinal.core.{Area, Bundle, Component, cloneOf, isPow2}
+import spinal.lib.bus.amba4.axi.{Axi4Config, Axi4Shared}
+import spinal.lib.memory.sdram._
+import spinal.lib.{master, slave}
+import spinal.lib.security.CAESARInterface
+
+/**
+  * Created by Jiangyi on 2019-07-10.
+  */
+
+object Axi4SharedSecurityCtrl {
+  def getAxiConfig(dataWidth: Int, idWidth: Int, layout: SdramLayout): Axi4Config = {
+    val widthFactor = dataWidth / layout.dataWidth
+
+      Axi4Config(
+        addressWidth = layout.byteAddressWidth,
+        dataWidth = dataWidth,
+        idWidth = idWidth,
+        useLock = false,
+        useRegion = false,
+        useCache = false,
+        useProt = false,
+        useQos = false
+      )
+  }
+}
+
+
+case class Axi4SharedSecurityCtrl(axiDataWidth: Int, axiIdWidth: Int, layout: SdramLayout, timing: SdramTimings, CAS: Int) extends Component {
+  val axiConfig = Axi4SharedSecurityCtrl.getAxiConfig(axiDataWidth, axiIdWidth, layout)
+
+  final val treeDepth = 4
+  val io = new Bundle {
+    val axi = slave(Axi4Shared(axiConfig))
+    val sdramAxi = master(Axi4Shared(axiConfig))
+  }
+
+  val crypto = new CAESARInterface()
+  crypto.connectToCryptoEngineToBus(io.axi)
+
+  io.axi <> io.sdramAxi
+
+  ???
+
+}
