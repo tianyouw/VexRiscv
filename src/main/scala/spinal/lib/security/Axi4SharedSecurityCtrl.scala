6fca6820eb23facab030e8dcbd9a05b8862fd5db
Andrew Jiang <andrew.jiang@edu.uwaterloo.ca>
2019-07-21 01:35:54 -0400

    Bypass memory tree read/writes

--- src/main/scala/spinal/lib/security/Axi4SharedSecurityCtrl.scala
+++ src/main/scala/spinal/lib/security/Axi4SharedSecurityCtrl.scala
@@ -28,7 +28,7 @@ object Axi4SharedSecurityCtrl {
 case class Axi4SharedSecurityCtrl(axiDataWidth: Int, axiAddrWidth: Int, axiIdWidth: Int, layout : SdramLayout) extends Component {
   def bytePosition(addr: UInt) : UInt = addr(4 downto 2)
 
-  def combineStrbDataWithOriginal(originalData: Bits, newData: Bits, strbMask: Bits) : Bits = {
+  def combineNewDataWithOriginal(originalData: Bits, newData: Bits, strbMask: Bits) : Bits = {
     assert(originalData.getWidth == newData.getWidth)
     assert(newData.getWidth == 32)
     val outData = newData
@@ -86,178 +86,184 @@ case class Axi4SharedSecurityCtrl(axiDataWidth: Int, axiAddrWidth: Int, axiIdWid
   caesarCtrl.io.out_stream <> dataOutFifo.io.push
   caesarCtrl.io.in_stream <> dataInFifo.io.pop
 
+  dataOutFifo.io.pop.ready := False
+
+  when (busyReg || io.axi.sharedCmd.addr < 0x4000000) {
+    dataOutFifo.io.pop.ready := io.sdramAxi.writeData.ready
 
 
-  dataOutFifo.io.pop.ready := io.sdramAxi.writeData.ready
+    io.sdramAxi.writeData.valid := dataOutFifo.io.pop.valid && sdramWrEnReg
+    io.sdramAxi.writeData.data := dataOutFifo.io.pop.payload.fragment
+    io.sdramAxi.writeData.strb := "1111"
+    io.sdramAxi.writeData.last := dataOutFifo.io.pop.payload.last
 
-  io.sdramAxi.writeData.valid := dataOutFifo.io.pop.valid && sdramWrEnReg
-  io.sdramAxi.writeData.data := dataOutFifo.io.pop.payload.fragment
-  io.sdramAxi.writeData.strb := "1111"
-  io.sdramAxi.writeData.last := dataOutFifo.io.pop.payload.last
-
-  io.sdramAxi.sharedCmd.size := io.axi.sharedCmd.size
-//    when (io.sdramAxi.sharedCmd.write) {
-//      io.sdramAxi.sharedCmd.len  := 0
-//      io.sdramAxi.sharedCmd.addr := axiSharedCmdReg.addr(axiConfig.addressWidth - 1 downto 5) @@ U"00000" + (pendingWordsCounter.value << 2)
-//
-//    } otherwise {
-  when (axiSharedCmdReg.write) {
-    io.sdramAxi.sharedCmd.len := 7
-  } otherwise  {
-    io.sdramAxi.sharedCmd.len := axiSharedCmdReg.len
-  }
-//      io.sdramAxi.sharedCmd.id := 8
-//    }
-  io.sdramAxi.sharedCmd.addr := axiSharedCmdReg.addr(axiConfig.addressWidth - 1 downto 5) @@ U"00000"
-  io.sdramAxi.sharedCmd.id := axiSharedCmdReg.id
-  io.sdramAxi.sharedCmd.burst := axiSharedCmdReg.burst
-  io.sdramAxi.sharedCmd.write := sdramWrEnReg
-  //    io.sdramAxi.sharedCmd.addr := io.axi.sharedCmd.addr
-  io.sdramAxi.sharedCmd.valid := sdramAxiSharedCmdValidReg
-  io.sdramAxi.writeRsp.ready := True
+    io.sdramAxi.sharedCmd.size := io.axi.sharedCmd.size
+    //    when (io.sdramAxi.sharedCmd.write) {
+    //      io.sdramAxi.sharedCmd.len  := 0
+    //      io.sdramAxi.sharedCmd.addr := axiSharedCmdReg.addr(axiConfig.addressWidth - 1 downto 5) @@ U"00000" + (pendingWordsCounter.value << 2)
+    //
+    //    } otherwise {
+    when(axiSharedCmdReg.write) {
+      io.sdramAxi.sharedCmd.len := 7
+    } otherwise {
+      io.sdramAxi.sharedCmd.len := axiSharedCmdReg.len
+    }
+    //      io.sdramAxi.sharedCmd.id := 8
+    //    }
+    io.sdramAxi.sharedCmd.addr := axiSharedCmdReg.addr(axiConfig.addressWidth - 1 downto 5) @@ U"00000"
+    io.sdramAxi.sharedCmd.id := axiSharedCmdReg.id
+    io.sdramAxi.sharedCmd.burst := axiSharedCmdReg.burst
+    io.sdramAxi.sharedCmd.write := sdramWrEnReg
+    //    io.sdramAxi.sharedCmd.addr := io.axi.sharedCmd.addr
+    io.sdramAxi.sharedCmd.valid := sdramAxiSharedCmdValidReg
+    io.sdramAxi.writeRsp.ready := True
 
 
-  io.axi.sharedCmd.ready := io.axi.sharedCmd.valid && !busyReg
-  io.axi.writeData.ready := io.axi.writeData.valid && !busyReg
-  io.axi.writeRsp.valid := writeRspValidReg
-  io.axi.writeRsp.resp := io.sdramAxi.writeRsp.resp
-  io.axi.writeRsp.payload.id := io.sdramAxi.writeRsp.payload.id
+    io.axi.sharedCmd.ready := io.axi.sharedCmd.valid && !busyReg
+    io.axi.writeData.ready := io.axi.writeData.valid && !busyReg
+    io.axi.writeRsp.valid := writeRspValidReg
+    io.axi.writeRsp.resp := io.sdramAxi.writeRsp.resp
+    io.axi.writeRsp.payload.id := io.sdramAxi.writeRsp.payload.id
 
-  io.axi.readRsp.valid := False
-  io.axi.readRsp.data := io.sdramAxi.readRsp.data
-  io.axi.readRsp.last := io.sdramAxi.readRsp.last
-  io.axi.readRsp.id := io.sdramAxi.readRsp.id
-  io.axi.readRsp.resp := io.sdramAxi.readRsp.resp
+    io.axi.readRsp.valid := False
+    io.axi.readRsp.data := io.sdramAxi.readRsp.data
+    io.axi.readRsp.last := io.sdramAxi.readRsp.last
+    io.axi.readRsp.id := io.sdramAxi.readRsp.id
+    io.axi.readRsp.resp := io.sdramAxi.readRsp.resp
 
-  io.sdramAxi.readRsp.ready := dataInFifo.io.push.ready
+    io.sdramAxi.readRsp.ready := dataInFifo.io.push.ready
 
-  val writeFsm = new StateMachine {
-    val idleState: State = new State with EntryPoint {
-      onEntry {
-        pendingWordsCounter := 0
-        sdramWrEnReg := False
-      }
+    val writeFsm = new StateMachine {
+      val idleState: State = new State with EntryPoint {
+        onEntry {
+          pendingWordsCounter := 0
+          sdramWrEnReg := False
+        }
 
-      whenIsActive {
-        when(io.axi.sharedCmd.fire) {
-          busyReg := True
-          axiSharedCmdReg := io.axi.sharedCmd
+        whenIsActive {
+          when(io.axi.sharedCmd.fire) {
+            busyReg := True
+            axiSharedCmdReg := io.axi.sharedCmd
 
-          // For the read case, go directly to read state
-          when (!io.axi.sharedCmd.write) {
+            // For the read case, go directly to read state
+            when(!io.axi.sharedCmd.write) {
+              goto(readState)
+            }
+          }
+
+          // These two blocks are for the write case
+          when(io.axi.writeData.fire) {
+            writeRspValidReg := True
+            dataReg := io.axi.writeData.data
+            strbReg := io.axi.writeData.strb
+            io.axi.writeRsp.isOKAY()
+          }
+
+          when(io.axi.writeRsp.fire) {
+            writeRspValidReg := False
             goto(readState)
           }
         }
 
-        // These two blocks are for the write case
-        when(io.axi.writeData.fire) {
-          writeRspValidReg := True
-          dataReg := io.axi.writeData.data
-          strbReg := io.axi.writeData.strb
-          io.axi.writeRsp.isOKAY()
-        }
-
-        when(io.axi.writeRsp.fire) {
-          writeRspValidReg := False
-          goto(readState)
+        onExit {
+          sdramAxiSharedCmdValidReg := True
         }
       }
 
-      onExit {
-        sdramAxiSharedCmdValidReg := True
-      }
-    }
-
-    val readState: State = new State {
-      //    onEntry(pendingWordsCounter := 0)
-      whenIsActive {
-          when (io.sdramAxi.sharedCmd.fire) {
+      val readState: State = new State {
+        //    onEntry(pendingWordsCounter := 0)
+        whenIsActive {
+          when(io.sdramAxi.sharedCmd.fire) {
             sdramAxiSharedCmdValidReg := False
           }
 
           io.sdramAxi.writeData.valid := False
-//          val axiSharedCmd = cloneOf(io.axi.sharedCmd)
-//
-//          axiSharedCmd.addr := io.axi.sharedCmd.addr(axiConfig.addressWidth - 1 downto 5) @@ U"00000" + (pendingWordsCounter.value << 2)
-//          axiSharedCmd.valid := True
-//          axiSharedCmd.write := False
-//
-//          io.sdramAxi.sharedCmd <> axiSharedCmd
+          //          val axiSharedCmd = cloneOf(io.axi.sharedCmd)
+          //
+          //          axiSharedCmd.addr := io.axi.sharedCmd.addr(axiConfig.addressWidth - 1 downto 5) @@ U"00000" + (pendingWordsCounter.value << 2)
+          //          axiSharedCmd.valid := True
+          //          axiSharedCmd.write := False
+          //
+          //          io.sdramAxi.sharedCmd <> axiSharedCmd
 
-        dataInFifo.io.push.payload.last := io.sdramAxi.readRsp.last
+          dataInFifo.io.push.payload.last := io.sdramAxi.readRsp.last
 
-        dataInFifo.io.push.valid := io.sdramAxi.readRsp.valid
-        when(axiSharedCmdReg.write && pendingWordsCounter.value === bytePosition(axiSharedCmdReg.addr)) {
-          dataInFifo.io.push.payload.fragment := combineStrbDataWithOriginal(io.sdramAxi.readRsp.data, dataReg, strbReg)
-        } otherwise {
-          dataInFifo.io.push.payload.fragment := io.sdramAxi.readRsp.data
-        }
+          dataInFifo.io.push.valid := io.sdramAxi.readRsp.valid
+          when(axiSharedCmdReg.write && pendingWordsCounter.value === bytePosition(axiSharedCmdReg.addr)) {
+            dataInFifo.io.push.payload.fragment := combineNewDataWithOriginal(io.sdramAxi.readRsp.data, dataReg, strbReg)
+          } otherwise {
+            dataInFifo.io.push.payload.fragment := io.sdramAxi.readRsp.data
+          }
 
 
-        when(dataInFifo.io.push.ready && io.sdramAxi.readRsp.fire) {
-//            io.sdramAxi.sharedCmd.valid := False
-          pendingWordsCounter.increment()
-//            sdramAxiSharedCmdValidReg := True
-          when(pendingWordsCounter.willOverflowIfInc) {
-            when(axiSharedCmdReg.write) {
-              sdramAxiSharedCmdValidReg := True
-              sdramWrEnReg := True
-              goto(writeState)
-            } otherwise {
-              goto(returnDataState)
+          when(dataInFifo.io.push.ready && io.sdramAxi.readRsp.fire) {
+            //            io.sdramAxi.sharedCmd.valid := False
+            pendingWordsCounter.increment()
+            //            sdramAxiSharedCmdValidReg := True
+            when(pendingWordsCounter.willOverflowIfInc) {
+              when(axiSharedCmdReg.write) {
+                sdramAxiSharedCmdValidReg := True
+                sdramWrEnReg := True
+                goto(writeState)
+              } otherwise {
+                goto(returnDataState)
+              }
             }
           }
         }
       }
-    }
 
-    val writeState: State = new State {
-      whenIsActive {
-        when (io.sdramAxi.sharedCmd.fire) {
-          sdramAxiSharedCmdValidReg := False
-        }
+      val writeState: State = new State {
+        whenIsActive {
+          when(io.sdramAxi.sharedCmd.fire) {
+            sdramAxiSharedCmdValidReg := False
+          }
 
-//          otherwise {
-//            io.sdramAxi.writeData.strb := "1111"
-//          }
+          //          otherwise {
+          //            io.sdramAxi.writeData.strb := "1111"
+          //          }
 
-        when(io.sdramAxi.writeRsp.fire) {
-//            pendingWordsCounter.increment()
-//            sdramAxiSharedCmdValidReg := True
-//            when(pendingWordsCounter.willOverflowIfInc) {
+          when(io.sdramAxi.writeRsp.fire) {
+            //            pendingWordsCounter.increment()
+            //            sdramAxiSharedCmdValidReg := True
+            //            when(pendingWordsCounter.willOverflowIfInc) {
             goto(idleState)
-//            }
+            //            }
+          }
+        }
+
+        onExit {
+          sdramWrEnReg := False
+          sdramAxiSharedCmdValidReg := False
+          busyReg := False
         }
       }
 
-      onExit {
-        sdramWrEnReg := False
-        sdramAxiSharedCmdValidReg := False
-        busyReg := False
+      // State for returning data to dcache
+      val returnDataState: State = new State {
+        whenIsActive {
+          io.axi.readRsp.valid := dataOutFifo.io.pop.valid
+          io.axi.readRsp.data := dataOutFifo.io.pop.payload.fragment
+          io.axi.readRsp.last := dataOutFifo.io.pop.payload.last
+          io.axi.readRsp.id := axiSharedCmdReg.id
+          io.axi.readRsp.setOKAY()
+
+          dataOutFifo.io.pop.ready := io.axi.readRsp.ready
+
+          when(io.axi.readRsp.fire && io.axi.readRsp.last) {
+            goto(idleState)
+          }
+        }
+        onExit {
+          sdramWrEnReg := False
+          sdramAxiSharedCmdValidReg := False
+          busyReg := False
+        }
       }
     }
-
-    // State for returning data to dcache
-    val returnDataState: State = new State {
-     whenIsActive {
-       io.axi.readRsp.valid := dataOutFifo.io.pop.valid
-       io.axi.readRsp.data := dataOutFifo.io.pop.payload.fragment
-       io.axi.readRsp.last := dataOutFifo.io.pop.payload.last
-       io.axi.readRsp.id := axiSharedCmdReg.id
-       io.axi.readRsp.setOKAY()
-
-       dataOutFifo.io.pop.ready := io.axi.readRsp.ready
-
-       when (io.axi.readRsp.fire && io.axi.readRsp.last) {
-         goto(idleState)
-       }
-     }
-     onExit {
-       sdramWrEnReg := False
-       sdramAxiSharedCmdValidReg := False
-       busyReg := False
-     }
-   }
+  } otherwise {
+    // Accessing tree section; bypass and let it through directly
+    io.axi <> io.sdramAxi
   }
 
 }
